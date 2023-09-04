ce6fc20ca2c8324b1eeebc2716218d1f60d9c18d
Paul Mihai Gansca <gansca.paul@gmail.com>
2023-09-04 20:44:26 +0300

    add fade animation to all sections

--- src/components/BreakTheMold.tsx
+++ src/components/BreakTheMold.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
+import { useInView } from 'react-intersection-observer';
 
 export const BreakTheMold = () => {
+  const [loadAnimationOnMount, setLoadAnimationOnMount] = React.useState(false);
+  const [ref, inView] = useInView({
+    /* Optional options */
+    threshold: 0.2,
+  });
+  React.useEffect(() => {
+    if (inView) setTimeout(() => setLoadAnimationOnMount(true), 10);
+    else setLoadAnimationOnMount(false);
+  }, [inView]);
   return (
-    <div className="flex flex-wrap lg:justify-center lg:items-center lg:px-40 lg:py-[20.5%] w-full pt-28 pb-20 px-7 sm:pb-56 sm:pt-48 sm:px-[7.5%] justify-around items-start bg-white">
+    <div
+      ref={ref}
+      className="break-the-mold flex flex-wrap lg:justify-center lg:items-center lg:px-40 lg:py-[20.5%] w-full pt-28 pb-20 px-7 sm:pb-56 sm:pt-48 sm:px-[7.5%] justify-around items-start bg-white"
+    >
       <div className="w-full flex flex-wrap justify-between items-center">
-        <h2 className="w-full max-w-full sm:w-[45%] text-5xl sm:text-6xl mb-12 sm:mb-6 font-bold font-title sm:max-w-[11ch] text-gray-900">
+        <h2
+          className={`w-full max-w-full sm:w-[45%] text-5xl sm:text-6xl mb-12 sm:mb-6 font-bold font-title sm:max-w-[11ch] text-gray-900 ${
+            loadAnimationOnMount ? 'mounted' : 'first-render'
+          }`}
+        >
           Beautiful websites that break the mold
         </h2>
-        <div className="sm:w-[45%] w-full font-body">
+        <div
+          className={`sm:w-[45%] w-full font-body ${
+            loadAnimationOnMount ? 'mounted' : 'first-render'
+          }`}
+        >
           <p className="text-xl mb-6 text-gray-900">
             Your website’s your opportunity to stand out. So why settle for
             something bland?
             <br />
-            At KOTA, our clients benefit from stunning sites that are carefully
+            At FLEX, our clients benefit from stunning sites that are carefully
             crafted and custom-made by some of the industry’s leading artistic
             minds.
           </p>
--- src/components/WhatMakesUsGoodSection.tsx
+++ src/components/WhatMakesUsGoodSection.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import { useInView } from 'react-intersection-observer';
 
 export const WhatMakesUsGoodSection = () => {
+  const [loadAnimationOnMount, setLoadAnimationOnMount] = React.useState(false);
+  const [ref, inView] = useInView({
+    /* Optional options */
+    threshold: 0.2,
+  });
+  React.useEffect(() => {
+    if (inView) setTimeout(() => setLoadAnimationOnMount(true), 10);
+    else setLoadAnimationOnMount(false);
+  }, [inView]);
   return (
-    <div className="lg:flex lg:gap-8 md:mt-16 sm:mt-44 bigPhone:mt-48 phone:mt-60 py-[6rem] sm:py-[10rem] px-[7.5%] bg-gradient-to-r from-fuchsia-600 to-pink-600 relative">
-      <p className="text-5xl lg:text-7xl lg:mb-0 lg:min-w-[33%] font-bold font-title mb-16 sm:mb-8 sm:w-[80%] sm:max-w-[15ch]">
+    <div
+      ref={ref}
+      className="what-makes-us-good lg:flex lg:gap-8 md:mt-16 sm:mt-44 bigPhone:mt-48 phone:mt-60 py-[6rem] sm:py-[10rem] px-[7.5%] bg-gradient-to-r from-fuchsia-600 to-pink-600 relative"
+    >
+      <p
+        className={`text-5xl lg:text-7xl lg:mb-0 lg:min-w-[33%] font-bold font-title mb-16 sm:mb-8 sm:w-[80%] sm:max-w-[15ch] ${
+          loadAnimationOnMount ? 'mounted' : 'first-render'
+        }`}
+      >
         What makes us sooo good at it…
       </p>
-      <div className="flex flex-wrap justify-between gap-5">
+      <div
+        className={`flex flex-wrap justify-between gap-5 ${
+          loadAnimationOnMount ? 'mounted' : 'first-render'
+        }`}
+      >
         <div className="sm:w-[48%]">
           <h3 className="text-3xl font-bold font-title mb-2">Bespoke</h3>
           <p className="font-body">
